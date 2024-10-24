c9dd70dc3333559e6fb155adb41674f911fe5a9b
Mogami <mogami.creative@gmail.com>
2024-10-24 22:41:31 +0200

    feat: domain changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import relativeLinks from "astro-relative-links";
-import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mogamihub.xyz',
+  site: 'https://mogamigit.github.io/goya',
   base: './',
-  integrations: [tailwind(), relativeLinks(), react()],
-  output: "server",
-  adapter: cloudflare()
+  integrations: [tailwind(), relativeLinks(), react()]
 });
