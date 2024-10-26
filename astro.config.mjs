ce2c9d35c4e25bef7f41222b461e37d679be8d87
Mogami <mogami.creative@gmail.com>
2024-10-26 20:17:02 +0200

    update dependencies, add postcss, rework layout, and remove unused components

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mogamigit.github.io/goya',
-  base: './',
   integrations: [tailwind(), relativeLinks(), react()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,18 +5,35 @@ export default {
 		'./node_modules/flowbite/**/*.js'
 	],
 	theme: {
-		extend: {
-			fontFamily: {
-				'kalnia': ['kalnia', 'Georgia', 'serif'],
-				'hubot': ['Hubot-Sans', 'Helvetica', 'sans-serif']
-			}
-		},
 		colors: {
-			'white': '#ffffff',
-			'blue-violet': '#4A68F0',
-			'yellow-chick': '#FFD121',
-			'dark-blue': '#222831',
+			amazon: {
+				'bg': '#00050D',
+				'gray': '#33373D',
+				'light-gray': '#B4B6B8',
+				'accent-yellow': '#FFD62C',
+				'accent-blue': '#1A98FF',
+				'prime': '#007DFD',
+				'green': '#37F1A3',
+				'links': '#90DFFE',
+				'gray-text': '#8197A4',
+			},
 		},
+		boxShadow: {
+			'glow': '0 0 10px rgba(51, 55, 61, 0.8), 0 0 30px rgba(51, 55, 61, 0.5)',
+		},
+		fontFamily: {
+			// Amazon
+			'roboto': ['Roboto', 'sans-serif'],
+		},
+		extend: {
+			spacing: {
+			  '8xl': '96rem',
+			  '9xl': '128rem',
+			},
+			borderRadius: {
+			  'xl': '10px',
+			}
+		}
 	},
 	plugins: [
 		require('flowbite/plugin'),
