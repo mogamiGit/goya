b62b2e1768911953c1b10a3f4789607e63f2d188
Mogami <mogami.creative@gmail.com>
2024-10-26 22:28:47 +0200

    Image JSX import prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mogamigit.github.io/goya',
+  base: './',
   integrations: [tailwind(), relativeLinks(), react()]
 });
