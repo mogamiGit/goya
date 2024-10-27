ea39ed4a5a5b93e6fbaffe54df96ed368e1fd88d
Mogami <mogami.creative@gmail.com>
2024-10-27 11:30:44 +0100

    implement npm astro icon package move assets and icon folder and change method images to .src and import

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import relativeLinks from "astro-relative-links";
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mogamigit.github.io/goya',
   base: '/goya',
-  integrations: [tailwind(), relativeLinks(), react()]
+  integrations: [tailwind(), relativeLinks(), react(), icon()]
 });
